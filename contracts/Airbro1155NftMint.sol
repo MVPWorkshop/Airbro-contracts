b6e0111dbbc87637b30cc18c771c8c7ab3938900
Stevan Bogosavljevic <stevyhacker@gmail.com>
2022-03-08 12:54:04 +0100

    Fix role modifiers

--- contracts/Airbro1155NftMint.sol
+++ contracts/Airbro1155NftMint.sol
@@ -12,11 +12,11 @@ contract AirBro1155NftMint is ERC1155, Ownable {
         _setURI(newUri);
     }
 
-    function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
+    function mint(address account, uint256 id, uint256 amount, bytes memory data) public {
         _mint(account, id, amount, data);
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public onlyOwner {
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public {
         _mintBatch(to, ids, amounts, data);
     }
 
