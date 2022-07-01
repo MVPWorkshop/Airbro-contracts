11f701fc71e259f7663d19da10e77a047ad6b07a
Aljosa Makevic <aljosa100@gmail.com>
2022-07-01 16:28:18 +0200

    added more tests for merkleTree functions in existing1155 for tw campaign

--- test/unit/airdrops1155Holder/Existing1155NftDrop1155/Existing1155NftDrop1155ShouldClaimReward.spec.ts
+++ test/unit/airdrops1155Holder/Existing1155NftDrop1155/Existing1155NftDrop1155ShouldClaimReward.spec.ts
@@ -2,13 +2,41 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 import { MerkleTree } from "merkletreejs"
 const { keccak256 } = ethers.utils
+import { constants } from "ethers";
 
 export const Existing1155NftDrop1155ShouldClaimReward = (): void => {
 
     describe('user should be able to claim reward', async function() {
         
         it("should be able to claim if part of merkleRoot", async function(){
+            const whitelisted = [this.signers.alice.address, this.signers.bob.address, this.signers.jerry.address, this.signers.lisa.address];
+            const leaves = whitelisted.map(addr => keccak256(addr))
+            const merkleTree = new MerkleTree(leaves, keccak256, { sort: true })
+            const roothash = merkleTree.getHexRoot();
 
+            expect(await this.existing1155NFTDrop1155.connect(this.signers.backendWallet).setMerkleRoot(roothash))
+            .to.emit(this.existing1155NFTDrop1155, "MerkleRootChanged").withArgs(roothash);
+
+            const hexProof = merkleTree.getHexProof(leaves[0]);
+
+            expect(await this.existing1155NFTDrop1155.connect(this.signers.alice).claim(hexProof))
+            .to.emit(this.existing1155NFTDrop1155, "Claimed").withArgs(this.signers.alice.address);
+        })
+
+        it("should revert claim if not part of merkleRoot", async function(){
+            const whitelisted = [this.signers.alice.address, this.signers.bob.address, this.signers.jerry.address, this.signers.lisa.address];
+            const leaves = whitelisted.map(addr => keccak256(addr))
+            const merkleTree = new MerkleTree(leaves, keccak256, { sort: true })
+            const roothash = merkleTree.getHexRoot();
+
+            expect(await this.existing1155NFTDrop1155.connect(this.signers.backendWallet).setMerkleRoot(roothash))
+            .to.emit(this.existing1155NFTDrop1155, "MerkleRootChanged").withArgs(roothash);
+
+            const hexProof = merkleTree.getHexProof(leaves[0]);
+
+            await expect(this.existing1155NFTDrop1155.connect(this.signers.peter)
+            .claim(hexProof))
+            .to.be.revertedWith(`NotEligible`);
         })
 
         it("should return true if eligible for reward", async function(){
@@ -25,7 +53,43 @@ export const Existing1155NftDrop1155ShouldClaimReward = (): void => {
 
             const isEligibleForReward = await this.existing1155NFTDrop1155.connect(this.signers.alice).isEligibleForReward(hexProof);
             
-            expect((isEligibleForReward)).to.equal(true);
+            expect(isEligibleForReward).to.equal(true);
+
+        })
+
+        it("should return airdropAmount if eligible for reward", async function(){
+
+            const whitelisted = [this.signers.alice.address, this.signers.bob.address, this.signers.jerry.address, this.signers.lisa.address];
+            const leaves = whitelisted.map(addr => keccak256(addr))
+            const merkleTree = new MerkleTree(leaves, keccak256, { sort: true })
+            const roothash = merkleTree.getHexRoot();
+
+            expect(await this.existing1155NFTDrop1155.connect(this.signers.backendWallet).setMerkleRoot(roothash))
+            .to.emit(this.existing1155NFTDrop1155, "MerkleRootChanged").withArgs(roothash);
+
+            const hexProof = merkleTree.getHexProof(leaves[0]);
+
+            const airdropAmount = await this.existing1155NFTDrop1155.connect(this.signers.alice).getAirdropAmount(hexProof);
+            
+            expect(airdropAmount).to.not.equal(constants.Zero);
+
+        })
+
+        it("should return 0 for airdropAmount if not eligible for reward", async function(){
+
+            const whitelisted = [this.signers.alice.address, this.signers.bob.address, this.signers.jerry.address, this.signers.lisa.address];
+            const leaves = whitelisted.map(addr => keccak256(addr))
+            const merkleTree = new MerkleTree(leaves, keccak256, { sort: true })
+            const roothash = merkleTree.getHexRoot();
+
+            expect(await this.existing1155NFTDrop1155.connect(this.signers.backendWallet).setMerkleRoot(roothash))
+            .to.emit(this.existing1155NFTDrop1155, "MerkleRootChanged").withArgs(roothash);
+
+            const hexProof = merkleTree.getHexProof(leaves[0]);
+
+            const airdropAmount = await this.existing1155NFTDrop1155.connect(this.signers.peter).getAirdropAmount(hexProof);
+            
+            expect(airdropAmount).to.equal(constants.Zero);
 
         })
     })
