a05e3eb037d94ff288a43d48e1d0a8774e20288d
Aljosa Makevic <aljosa100@gmail.com>
2022-07-20 10:41:25 +0200

    adding airdropCampaignData contract

---
+++ contracts/AirdropCampaignData.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+/// @title AirdropCampaignData - Data contract for storing of daily merkleRootHashes of airbro Campaigns
+contract AirdropCampaignData {
+    address public admin = 0xF4b5bFB92dD4E6D529476bCab28A65bb6B32EFb3;
+
+    enum Chains {
+        Zero,
+        Eth,
+        Pol
+    }
+
+    struct AirdropData {
+        Chains chain;
+        bytes32[] hashArray;
+    }
+
+    mapping(address => AirdropData) public airdrops;
+
+    error NotAdmin();
+    error UnequalArrays();
+
+    event AdminChanged(address adminAddress);
+    event MerkleRootHashAdded(address indexed airdropCampaignAddress, bytes32 indexed merkleRootHash);
+    event ChainAdded(address indexed airdropCampaignAddress, Chains indexed airdropChain);
+
+    modifier onlyAdmin() {
+        if (msg.sender != admin) revert NotAdmin();
+        _;
+    }
+
+    constructor() {}
+
+    /// @notice Updates the address of the admin variable
+    /// @param _newAdmin - New address for the admin of this contract, and the address for all newly created airdrop contracts
+    function changeAdmin(address _newAdmin) external onlyAdmin {
+        admin = _newAdmin;
+        emit AdminChanged(_newAdmin);
+    }
+
+    /// @notice Adds daily merkle root hash for any airdropCampaign contract
+    /// @param _airdropCampaignAddress - address of the airdropCampaign contract whose current participants are in the daily merkleRootHash
+    /// @param _merkleRootHash - merkle root hash of daily participants of an airdropCampaign
+    function addDailyMerkleRootHash(address _airdropCampaignAddress, bytes32 _merkleRootHash) external onlyAdmin {
+        airdrops[_airdropCampaignAddress].hashArray.push(_merkleRootHash);
+        emit MerkleRootHashAdded(_airdropCampaignAddress, _merkleRootHash);
+    }
+
+    /// @notice Adds array of daily merkle root hashes for multiple airdropCampaign contracts
+    /// @param _airdropCampaignAddressArray - array of addresses of multiple airdropCampaign contracts whose current participants are in the daily merkleRootHash
+    /// @param _merkleRootHashArray - array of merkle root hashes of daily participants of multiple airdropCampaigns
+    function batchAddDailyMerkleRootHash(address[] memory _airdropCampaignAddressArray, bytes32[] memory _merkleRootHashArray)
+        external
+        onlyAdmin
+    {
+        if (_airdropCampaignAddressArray.length != _merkleRootHashArray.length) revert UnequalArrays();
+
+        uint256 arrayLength = _airdropCampaignAddressArray.length;
+
+        for (uint256 i; i < arrayLength; i++) {
+            airdrops[_airdropCampaignAddressArray[i]].hashArray.push(_merkleRootHashArray[i]);
+            emit MerkleRootHashAdded(_airdropCampaignAddressArray[i], _merkleRootHashArray[i]);
+        }
+    }
+
+    /// @notice Adds string "ETH" or "POL" to mapping depending on which network the airdropCampaign contract is on
+    /// @param _airdropCampaignAddress - address of airdropCampaign contract
+    /// @param _airdropChain - string representing blockchain Chain
+    function addAirdropCampaignChain(address _airdropCampaignAddress, Chains _airdropChain) external onlyAdmin {
+        airdrops[_airdropCampaignAddress].chain = _airdropChain;
+        emit ChainAdded(_airdropCampaignAddress, _airdropChain);
+    }
+
+    /// @notice Adds array of Chain info: "Eth" or "Pol" to mapping depending on which Chain the airdropCampaign contracts are on
+    /// @param _airdropCampaignAddressArray - address of airdropCampaign contract
+    /// @param _airdropChainArray - array of uints representing blockchain chain
+    function batchAddAirdropCampaignChain(address[] memory _airdropCampaignAddressArray, Chains[] memory _airdropChainArray)
+        external
+        onlyAdmin
+    {
+        if (_airdropCampaignAddressArray.length != _airdropChainArray.length) revert UnequalArrays();
+
+        uint256 arrayLength = _airdropCampaignAddressArray.length;
+
+        for (uint256 i; i < arrayLength; i++) {
+            airdrops[_airdropCampaignAddressArray[i]].chain = _airdropChainArray[i];
+            emit ChainAdded(_airdropCampaignAddressArray[i], _airdropChainArray[i]);
+        }
+    }
+}
