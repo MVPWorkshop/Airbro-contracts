71fff2601555d0fa82e3eae461171be19f68e46c
nlukic97 <nlukic97@gmail.com>
2022-07-21 11:08:04 +0200

    made setMerkleRoot on New1155DropCampaign to only be setable once, and added tests

--- test/integration/AirbroFactoryCampaign/AirbroCampaignFactoryShouldChangeAdmin.spec.ts
+++ test/integration/AirbroFactoryCampaign/AirbroCampaignFactoryShouldChangeAdmin.spec.ts
@@ -16,13 +16,23 @@ export function AirbroCampaignFactoryShouldChangeAdminInAllAirDrops(): void {
     const merkleTree = new MerkleTree(leaves, keccak256, { sort: true });
     const roothash = merkleTree.getHexRoot();
 
-    // checking if initial admin address is able to set MerkleRootHash
+    // checking if non-admin account is able to set MerkleRootHash
+    await expect(this.newERC1155DropCampaign.connect(this.signers.alice).setMerkleRoot(bytes32MerkleRootHash)).to.be.revertedWith(
+      `Unauthorized`,
+    );
+
+    // checking if admin address is able to set MerkleRootHash
     expect(await this.newERC1155DropCampaign.connect(this.signers.backendWallet).setMerkleRoot(roothash))
       .to.emit(this.newERC1155DropCampaign, "MerkleRootChanged")
       .withArgs(roothash);
+  });
 
-    // making merkleProof for alice's address
-    const hexProof = merkleTree.getHexProof(leaves[0]);
+  it("new admin should be able to set merkleRoot", async function () {
+    // making merkleTree and merkleRootHash
+    const whitelisted = [this.signers.alice.address, this.signers.bob.address, this.signers.jerry.address, this.signers.lisa.address];
+    const leaves = whitelisted.map(addr => keccak256(addr));
+    const merkleTree = new MerkleTree(leaves, keccak256, { sort: true });
+    const roothash = merkleTree.getHexRoot();
 
     // changing admin address in the airbroCampaignFactory Contract
     expect(await this.airbroCampaignFactory.connect(this.signers.backendWallet).changeAdmin(this.signers.lisa.address))
@@ -32,13 +42,13 @@ export function AirbroCampaignFactoryShouldChangeAdminInAllAirDrops(): void {
     expect(await this.airbroCampaignFactory.admin()).to.be.equal(this.signers.lisa.address);
 
     // checking if old admin can set MerkleRootHash
-    await expect(this.newERC1155DropCampaign.connect(this.signers.backendWallet).setMerkleRoot(bytes32MerkleRootHash)).to.be.revertedWith(
+    await expect(this.newERC1155DropCampaign.connect(this.signers.backendWallet).setMerkleRoot(roothash)).to.be.revertedWith(
       `Unauthorized`,
     );
 
     // checking if new admin address is able to set MerkleRootHash
-    expect(await this.newERC1155DropCampaign.connect(this.signers.lisa).setMerkleRoot(bytes32MerkleRootHash))
+    expect(await this.newERC1155DropCampaign.connect(this.signers.lisa).setMerkleRoot(roothash))
       .to.emit(this.newERC1155DropCampaign, "MerkleRootChanged")
-      .withArgs(bytes32MerkleRootHash);
+      .withArgs(roothash);
   });
 }
