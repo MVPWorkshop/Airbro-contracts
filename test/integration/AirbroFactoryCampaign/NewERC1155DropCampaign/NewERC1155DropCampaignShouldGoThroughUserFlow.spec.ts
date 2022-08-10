a323727ae77df202a0e15ada146aa574ce8e9a65
nlukic97 <nlukic97@gmail.com>
2022-08-10 14:37:50 +0100

    fixed error for test

--- test/integration/AirbroFactoryCampaign/NewERC1155DropCampaign/NewERC1155DropCampaignShouldGoThroughUserFlow.spec.ts
+++ test/integration/AirbroFactoryCampaign/NewERC1155DropCampaign/NewERC1155DropCampaignShouldGoThroughUserFlow.spec.ts
@@ -23,44 +23,44 @@ export function NewERC1155DropCampaignShouldGoThroughUserFlow() {
       "Unauthorized",
     );
   });
-  // for some reason it is showing an error but all is fine and defined
-  // it("should test newERC1155DropCampaign Contract flow", async function () {
-  //   await expect(this.airdropRegistry.connect(this.signers.registryAdmin).addFactory(this.airbroCampaignFactory.address))
-  //     .to.emit(this.airdropRegistry, "FactoryWhitelisted")
-  //     .withArgs(this.airbroCampaignFactory.address);
+  
+  it("should test newERC1155DropCampaign Contract flow", async function () {
+    await expect(this.airdropRegistry.connect(this.signers.registryAdmin).addFactory(this.airbroCampaignFactory.address))
+      .to.emit(this.airdropRegistry, "FactoryWhitelisted")
+      .withArgs(this.airbroCampaignFactory.address);
 
-  //   // creating the NewERC1155DropCampaign from the factory contract
-  //   await this.airbroCampaignFactory.connect(this.signers.deployer).createNewERC1155DropCampaign(uri);
-  //   const newERC1155DropCampaignFactory = await ethers.getContractFactory("NewERC1155DropCampaign");
-  //   const NewERC1155DropCampaignContract = newERC1155DropCampaignFactory.attach(await this.airdropRegistry.airdrops(constants.Zero));
+    // creating the NewERC1155DropCampaign from the factory contract
+    await this.airbroCampaignFactory.connect(this.signers.deployer).createNewERC1155DropCampaign(uri);
+    const newERC1155DropCampaignFactory = await ethers.getContractFactory("NewERC1155DropCampaign");
+    const NewERC1155DropCampaignContract = newERC1155DropCampaignFactory.attach(await this.airdropRegistry.airdrops(constants.Zero));
 
-  //   //create merkleRootHash
-  //   const whitelisted = [this.signers.alice.address, this.signers.bob.address, this.signers.jerry.address];
-  //   const leaves = whitelisted.map(addr => keccak256(addr));
-  //   const merkleTree = new MerkleTree(leaves, keccak256, { sort: true });
-  //   const roothash = merkleTree.getHexRoot();
+    //create merkleRootHash
+    const whitelisted = [this.signers.alice.address, this.signers.bob.address, this.signers.jerry.address];
+    const leaves = whitelisted.map(addr => keccak256(addr));
+    const merkleTree = new MerkleTree(leaves, keccak256, { sort: true });
+    const roothash = merkleTree.getHexRoot();
 
-  //   //backendWallet sets new merkleRootHash upon completion of the campaign (deadline has passed)
-  //   expect(await NewERC1155DropCampaignContract.connect(this.signers.backendWallet).setMerkleRoot(roothash))
-  //     .to.emit(this.NewERC1155DropCampaignContract, "MerkleRootSet")
-  //     .withArgs(roothash);
+    //backendWallet sets new merkleRootHash upon completion of the campaign (deadline has passed)
+    expect(await NewERC1155DropCampaignContract.connect(this.signers.backendWallet).setMerkleRoot(roothash))
+      .to.emit(NewERC1155DropCampaignContract, "MerkleRootSet")
+      .withArgs(roothash);
 
-  //   //create Merkle Proof for alice
-  //   const hexProof = merkleTree.getHexProof(leaves[0]);
+    //create Merkle Proof for alice
+    const hexProof = merkleTree.getHexProof(leaves[0]);
 
-  //   // alice withdrawing 1155 on basis of her address being included in the merkleRoot
-  //   expect(await NewERC1155DropCampaignContract.connect(this.signers.alice).claim(hexProof, { value: claimFee }))
-  //     .to.emit(NewERC1155DropCampaignContract, "Claimed")
-  //     .withArgs(this.signers.alice.address);
+    // alice withdrawing 1155 on basis of her address being included in the merkleRoot
+    expect(await NewERC1155DropCampaignContract.connect(this.signers.alice).claim(hexProof, { value: claimFee }))
+      .to.emit(NewERC1155DropCampaignContract, "Claimed")
+      .withArgs(this.signers.alice.address);
 
-  //   // alice trying to withdraw twice
-  //   await expect(NewERC1155DropCampaignContract.connect(this.signers.alice).claim(hexProof, { value: claimFee })).to.be.revertedWith(
-  //     "AlreadyRedeemed",
-  //   );
+    // alice trying to withdraw twice
+    await expect(NewERC1155DropCampaignContract.connect(this.signers.alice).claim(hexProof, { value: claimFee })).to.be.revertedWith(
+      "AlreadyRedeemed",
+    );
 
-  //   // address that is not in merkleRootHash trying to withdraw
-  //   await expect(NewERC1155DropCampaignContract.connect(this.signers.lisa).claim(hexProof, { value: claimFee })).to.be.revertedWith(
-  //     "NotEligible",
-  //   );
-  // });
+    // address that is not in merkleRootHash trying to withdraw
+    await expect(NewERC1155DropCampaignContract.connect(this.signers.lisa).claim(hexProof, { value: claimFee })).to.be.revertedWith(
+      "NotEligible",
+    );
+  });
 }
