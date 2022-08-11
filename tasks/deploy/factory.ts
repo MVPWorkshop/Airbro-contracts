5ac45a8f8e13de6341a81da063161004db499831
Aljosa Makevic <aljosa100@gmail.com>
2022-08-11 13:20:20 +0100

    updated deploy script

--- tasks/deploy/factory.ts
+++ tasks/deploy/factory.ts
@@ -1,6 +1,9 @@
 import { task } from "hardhat/config";
 import { TaskArguments } from "hardhat/types";
 
+import { AirdropRegistry } from "../../src/types/contracts/AirdropRegistry";
+import { AirdropRegistry__factory } from "../../src/types/factories/contracts/AirdropRegistry__factory";
+
 import { AirbroCampaignFactory } from "../../src/types/contracts/AirbroCampaignFactory";
 import { AirbroCampaignFactory__factory } from "../../src/types/factories/contracts/AirbroCampaignFactory__factory";
 
@@ -8,27 +11,48 @@ import { Signer } from "@ethersproject/abstract-signer";
 import { isAddress } from "ethers/lib/utils";
 
 task("deploy:AirbroCampaignFactory").setAction(async function (taskArguments: TaskArguments, { ethers }) {
-  const BACKEND_WALLET_ADDRESS: string | undefined = process.env.BACKEND_WALLET_ADDRESS;
-  if (BACKEND_WALLET_ADDRESS === undefined || BACKEND_WALLET_ADDRESS === "") {
-    throw new Error("Please define the BACKEND_WALLET_ADDRESS in your .env file.");
+  const REGISTRY_ADMIN_WALLET_ADDRESS: string | undefined = process.env.REGISTRY_ADMIN_WALLET_ADDRESS;
+  if (REGISTRY_ADMIN_WALLET_ADDRESS === undefined || REGISTRY_ADMIN_WALLET_ADDRESS === "") {
+    throw new Error("Please define the REGISTRY_ADMIN_WALLET_ADDRESS in your .env file.");
   }
   const TREASURY_WALLET_ADDRESS: string | undefined = process.env.TREASURY_WALLET_ADDRESS;
   if (TREASURY_WALLET_ADDRESS === undefined || TREASURY_WALLET_ADDRESS === "") {
     throw new Error("Please define the TREASURY_WALLET_ADDRESS in your .env file.");
   }
+  const BACKEND_WALLET_ADDRESS: string | undefined = process.env.BACKEND_WALLET_ADDRESS;
+  if (BACKEND_WALLET_ADDRESS === undefined || BACKEND_WALLET_ADDRESS === "") {
+    throw new Error("Please define the BACKEND_WALLET_ADDRESS in your .env file.");
+  }
 
   const accounts: Signer[] = await ethers.getSigners();
+
+  console.log("Airdrop Registry:");
+
   console.log("Deployer address: " + (await accounts[0].getAddress()));
-  console.log("Constuctor args: " + BACKEND_WALLET_ADDRESS);
+  console.log("Constuctor args: " + REGISTRY_ADMIN_WALLET_ADDRESS, TREASURY_WALLET_ADDRESS);
+
+  const AirdropRegistryFactory: AirdropRegistry__factory = <AirdropRegistry__factory>await ethers.getContractFactory("AirdropRegistry");
+
+  const airdropRegistry: AirdropRegistry = <AirdropRegistry>(
+    await AirdropRegistryFactory.deploy(REGISTRY_ADMIN_WALLET_ADDRESS, TREASURY_WALLET_ADDRESS)
+  );
+  await airdropRegistry.deployed();
+
+  console.log("Airdrop Registry deployed to: ", airdropRegistry.address);
+
+  console.log("AirbroCampaignFactory:");
+
+  console.log("Deployer address: " + (await accounts[0].getAddress()));
+  console.log("Constuctor args: " + BACKEND_WALLET_ADDRESS, airdropRegistry.address);
 
   const AirbroCampaignFactoryFactory: AirbroCampaignFactory__factory = <AirbroCampaignFactory__factory>(
     await ethers.getContractFactory("AirbroCampaignFactory")
   );
-  // for some reason TREASURY_WALLET_ADDRESS here is displayed as an error
-  const AirbroCampaignFactory: AirbroCampaignFactory = <AirbroCampaignFactory>(
-    await AirbroCampaignFactoryFactory.deploy(BACKEND_WALLET_ADDRESS, TREASURY_WALLET_ADDRESS)
-  );
-  await AirbroCampaignFactory.deployed();
 
-  console.log("Airbro Campaign Factory deployed to: ", AirbroCampaignFactory.address);
+  const airbroCampaignFactory: AirbroCampaignFactory = <AirbroCampaignFactory>(
+    await AirbroCampaignFactoryFactory.deploy(BACKEND_WALLET_ADDRESS, airdropRegistry.address)
+  );
+  await airbroCampaignFactory.deployed();
+
+  console.log("Airbro Campaign Factory deployed to: ", airbroCampaignFactory.address);
 });
